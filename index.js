32267a8b5cf5e853643eda8b0352a719f5126975
Chris Kirk <cperryk@gmail.com>
2018-02-28 13:55:43 -0500

    Simplifies index code

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 'use strict';
 
-const getDocs = require('./lib/get-docs'),
-  putDocs = require('./lib/put-docs'),
-  initCmd = require('./cmd/init-cmd'),
+const initCmd = require('./cmd/init-cmd'),
   runningAsScript = !module.parent;
 
 if (runningAsScript) initCmd();
-module.exports.getDocs = getDocs;
-module.exports.putDocs = putDocs;
+module.exports.get = require('./lib/put-docs');
+module.exports.put = require('./lib/get-docs');
