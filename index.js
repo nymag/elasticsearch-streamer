00fa29bea458034526ca198161ef0ae305cf3417
Chris Kirk <cperryk@gmail.com>
2018-06-04 11:00:35 -0400

    Fix programmatic exports

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const initCmd = require('./cmd/init-cmd'),
   runningAsScript = !module.parent;
 
 if (runningAsScript) initCmd();
-module.exports.get = require('./lib/put-docs');
-module.exports.put = require('./lib/get-docs');
+module.exports.get = require('./lib/get-docs');
+module.exports.put = require('./lib/put-docs');
